67ad624dcfacc9f16e354b2fb0d4405139a158c8
Fadly Kayo <f_kayo@yahoo.com>
2017-02-01 19:39:58 +0700

    Add some features

--- routes/api/gamedb/home.js
+++ routes/api/gamedb/home.js
@@ -8,21 +8,22 @@ var youtube = new YouTube()
 youtube.setKey(config.youtube_key)
 process.env.mashapeKey = config.mashape_key
 
-router.get('/', function (req, res) {
+router.get('/:key', function (req, res) {
+  let opt = req.params.key
   getGames(function (list) {
     getTrailers(list, function (data) {
       res.send(data)
     })
-  })
+  }, opt)
 })
 
-const getGames = (cb) => {
+const getGames = (cb, test) => {
   igdb.games({
-    limit: 25,
+    limit: 50,
     offset: 0,
-    fields: 'name',
-    order: 'release_dates.date:desc',
-    search: 'marvel'
+    fields: 'name,rating,popularity,url',
+    order: 'rating:desc',
+    search: test
   }).then(function (games) {
     cb(games.body)
   }).catch(function (err) {
@@ -32,15 +33,15 @@ const getGames = (cb) => {
 
 const getTrailers = (list, cb) => {
   let items = []
-  // console.log(list)
   list.forEach(function (video) {
-    // console.log(video.name)
-    youtube.search(`${video.name} trailer`, 1, function (error, result) {
+    youtube.search(`${video.name} game trailer`, 1, function (error, result) {
       items.push({
         name: video.name,
+        rating: video.rating,
+        popularity: video.popularity,
+        url: video.url,
         youtube: `https://youtu.be/${result.items[0].id.videoId}`
       })
-      // console.log(urls)
       if (items.length === list.length) cb(items)
     })
   })
